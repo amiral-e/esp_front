46b5b1f63b7201f0876483e93805f8c01e8053d8
Samoten777 <laure@alobees.com>
2024-09-20 11:36:32 +0200

    wip: github oauth

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,9 +1,13 @@
+
 import { signInAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator"
+
 import Link from "next/link";
+import { GithubSSO } from '@/components/github-sso'
 
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
@@ -17,27 +21,35 @@ export default function Login({ searchParams }: { searchParams: Message }) {
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
+        <Input name="email" placeholder="you@example.com" required/>
         <div className="flex justify-between items-center">
           <Label htmlFor="password">Password</Label>
           <Link
-            className="text-xs text-foreground underline"
-            href="/forgot-password"
+              className="text-xs text-foreground underline"
+              href="/forgot-password"
           >
             Forgot Password?
           </Link>
         </div>
         <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
+            type="password"
+            name="password"
+            placeholder="Your password"
+            required
         />
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
           Sign in
         </SubmitButton>
-        <FormMessage message={searchParams} />
+        <FormMessage message={searchParams}/>
+
+
+        <Separator/>
+
+        <p className="text-sm text-foreground">
+          Or sign in with SSO :
+        </p>
+        <GithubSSO />
       </div>
     </form>
-  );
+);
 }
---
+++ components/github-sso.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { signInWithGithub } from "@/app/oauth";
+
+export const GithubSSO = () => (
+    <div className="flex items-center gap-3">
+        <div
+            onClick={signInWithGithub}
+            className="relative flex items-center justify-center shrink-0 h-10 w-full rounded-lg group cursor-pointer overflow-hidden border hover:border-strong bg-surface-100 transition">
+            <img className="w-5" src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
+                 alt="GitHub Icon"/>
+            <div className="flex flex-col gap-1">
+                <div className="flex items-center gap-3">
+                    <h5 className="text-base text-foreground m-0">GitHub</h5>
+                </div>
+
+            </div>
+        </div>
+    </div>
+)
