3fd109c3c2a2db0b499493f8c36189f3776178e5
Matteo Linzi <matteo.linzi@epitech.eu>
2024-09-20 14:04:01 +0200

    style: format files

--- app/oauth.ts
+++ app/oauth.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@/utils/supabase/client";
 
 export const signInWithGithub = async () => {
-    const supabase = createClient();
+	const supabase = createClient();
 
-    await supabase.auth.signInWithOAuth({
-        provider: 'github',
-        options: { redirectTo: `${process.env.NEXT_PUBLIC_HOMEPAGE_URL}/auth/callback` }
-    })
-}
+	await supabase.auth.signInWithOAuth({
+		provider: "github",
+		options: {
+			redirectTo: `${process.env.NEXT_PUBLIC_HOMEPAGE_URL}/auth/callback`,
+		},
+	});
+};
