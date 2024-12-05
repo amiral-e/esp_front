5c5452a9ead48681be40d56ecd0ef1e6dca4f0f6
Faniry <faniry.ramavozatovo@ouidou.fr>
2024-12-05 14:26:19 +0100

    feat:call API conversations

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import "./conversation/chat.css"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
