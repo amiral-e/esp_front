1ccd5707e401ee9a1a78600605e12e3cf92096fc
Faniry <faniry.ramavozatovo@ouidou.fr>
2025-02-28 16:48:23 +0100

    fix:build

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function PaymentSuccess() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <PaymentSuccessContent />
+    </Suspense>
+  );
+}
+
+function PaymentSuccessContent() {
   const searchParams = useSearchParams();
   const amount = searchParams.get("amount") || "0";
 
@@ -11,11 +20,14 @@ export default function PaymentSuccess() {
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
         <h2 className="text-2xl">You successfully sent</h2>
-
         <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
           ${amount}
         </div>
       </div>
     </main>
   );
+}
+
+function Loading() {
+  return <p className="text-center text-white">Loading...</p>;
 }
